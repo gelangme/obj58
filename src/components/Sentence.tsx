3c1877d120f5e76699e99d508269113e90233338
web3skeptic <44979695+web3skeptic@users.noreply.github.com>
2024-08-01 09:50:41 +0200

    feat: add headers

--- src/components/Sentence.tsx
+++ src/components/Sentence.tsx
@@ -1,23 +1,36 @@
 import { iSentence } from "@/common/types";
 import Word from "./Word";
-const ignor_words = [".", ",", "?", "!", ":"]; // Array of symbols and words which are not higlighted
+const ignor_words = [".", ",", "?", "!", ":", ")"]; // Array of symbols and words which are not higlighted
 
 export default function Sentence({ sentence }: { sentence: iSentence }) {
   return (
     <div className="mb-4">
-      <div>
-        {sentence.words.map((item, item_index) => {
-          const isIgnored = ignor_words.includes(item.inf);
-          return (
-            <span key={item_index}>
-              <Word word={item} noTooltip={isIgnored} whiteSpace={item_index !== 0 && !isIgnored} />
-            </span>
-          );
-        })}
-      </div>
-      <span className="opacity-10 hover:opacity-50 transition-opacity duration-300" title="Translation">
+      {sentence.type === "h2" ? (
+        <h2 className="text-2xl font-bold">
+          {sentence.words.map((item, item_index) => {
+            const isIgnored = ignor_words.includes(item.inf);
+            return (
+              <span key={item_index}>
+                <Word word={item} noTooltip={isIgnored} whiteSpace={item_index !== 0 && !isIgnored} />
+              </span>
+            );
+          })}
+        </h2>
+      ) : (
+        <span>
+          {sentence.words.map((item, item_index) => {
+            const isIgnored = ignor_words.includes(item.inf);
+            return (
+              <span key={item_index}>
+                <Word word={item} noTooltip={isIgnored} whiteSpace={item_index !== 0 && !isIgnored} />
+              </span>
+            );
+          })}
+        </span>
+      )}
+      <div className="opacity-10 hover:opacity-50 transition-opacity duration-300" title="Translation">
         {sentence.translation}
-      </span>
+      </div>
     </div>
   );
 }
